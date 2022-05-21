3952b8c5fe2cbac68e57aed077e630a0b0dacd45
JamalRonin <jamalelk60@gmail.com>
2022-05-21 12:59:08 +0200

    css and js ok

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -6,7 +6,28 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/bootstrap/bootstrap.min.css';
+import './styles/bootstrap-icons/bootstrap-icons.css';
+import  './styles/boxicons/css/boxicons.min.css';
+import './styles/glightbox/glightbox.min.css';
+import './styles/remixicon/remixicon.css';
+import './styles/swiper-bundle.min.css';
+import bg from './img/bg.jpg';
+import './styles/style.css';
+
+
+
+import './purecounter';
+import './bootsrap/bootstrap.bundle.min.js';
+import './glightbox/glightbox.min.js';
+// import './isotope-layout/isotope.pkgd.min.js';
+import './swiper-bundle.min.js';
+import './noframework.waypoints.js';
+import './validate.js';
+import './main.js';
+
 
 // start the Stimulus application
 import './bootstrap';
+
+console.log('Hi! My name is app.js');
