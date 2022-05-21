5ad99ffdb9191a110df2775a58d596ee45871dd0
JamalRonin <jamalelk60@gmail.com>
2022-05-21 17:17:33 +0200

    fragmentation code

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\InfoNumber;
+use App\Entity\Interests;
+use App\Repository\AboutRepository;
+use App\Repository\InfoNumberRepository;
+use App\Repository\InterestsRepository;
+use App\Repository\NavRepository;
+use App\Repository\SocialLinkRepository;
+use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +19,23 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
-    {
+    public function index(AboutRepository $aboutRepository, InfoNumberRepository $infoNumberRepository, InterestsRepository $interestsRepository, NavRepository $navRepository, SocialLinkRepository $socialLinkRepository, TeamRepository $teamRepository): Response
+    {   
+        $about = $aboutRepository->findAll();
+        $infoNumber = $infoNumberRepository->findAll();
+        $interests = $interestsRepository->findAll();
+        $nav = $navRepository->findAll();
+        $social = $socialLinkRepository->findAll();
+        $team = $teamRepository->findAll();
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'about' => $about,
+            'infoNumber' => $infoNumber, 
+            'interests' => $interests,
+            'nav' => $nav, 
+            'social' => $social,
+            'team' => $team,
         ]);
     }
 }
