ac2210fc5f463a9611f8bb48dc5018768eff2218
JamalRonin <jamalelk60@gmail.com>
2022-07-20 17:52:22 +0200

    https://trello.com/c/v4rr5qpm

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -12,17 +12,39 @@ class AppFixtures extends Fixture
     {
         $siteVitrine = new Produit();
         $siteVitrine->setNom('Site vitrine');
-        $siteVitrine->setDescription('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionUne('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionDeux('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionTrois('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionQuatre('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionCinq('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionSix('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionSept('Site vitrine pour un site vitrine');
-        $siteVitrine->setDescriptionHuit('Site vitrine pour un site vitrine');
-        $siteVitrine->setPrix('100');
+        $siteVitrine->setDescription('Site vitrine responsive');
+        $siteVitrine->setDescriptionUne('backoffice est personnalisé');
+        $siteVitrine->setDescriptionDeux('suivis durant 12 mois');
+        $siteVitrine->setDescriptionTrois('évolutif');
+        $siteVitrine->setDescriptionQuatre('documenter');
+        $siteVitrine->setDescriptionCinq('Propriétaire complètement sur votre boutique');
+        $siteVitrine->setDescriptionSix(' Notifications de prise de contact par mail');
+        $siteVitrine->setDescriptionSept('réponse aux clients automatique');
+        $siteVitrine->setPrix('149 € sur 12 mois');
+        $siteVitrine->setClass('bx bxl-chrome');
         $manager->persist($siteVitrine);
+
+        $siteEcommerce = new Produit();
+        $siteEcommerce->setNom('Site e-commerces');
+        $siteEcommerce->setDescription('Site e-commerces responsive');
+        $siteEcommerce->setDescriptionUne('backoffice est personnalisé');
+        $siteEcommerce->setDescriptionDeux('suivis durant 12 mois');
+        $siteEcommerce->setDescriptionTrois('évolutif');
+        $siteEcommerce->setDescriptionQuatre('documenter');
+        $siteEcommerce->setDescriptionCinq('Propriétaire complètement sur votre boutique');
+        $siteEcommerce->setDescriptionSix(' Notification de panier, notification de page abandonner et achat par mail. ');
+        $siteEcommerce->setDescriptionSept('réponse aux clients automatique');
+        $siteEcommerce->setPrix('339 € sur 12 mois');
+        $siteEcommerce->setClass('bx bxs-store-alt');
+        $manager->persist($siteEcommerce);
+
+        $freelance = new Produit();
+        $freelance->setNom('FreeLance');
+        $freelance->setDescription('Mise à disposition de mes compétences à la journée');
+        $freelance->setPrix('359 € Jours');
+        $freelance->setClass('bx bxs-hourglass');
+        $manager->persist($freelance);
+
 
         $manager->flush();
     }
